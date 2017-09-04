b3446f316af830f009ff73320ffc033e1b603441
devikaparvathy <31448741+devikaparvathy@users.noreply.github.com>
2017-09-04 23:33:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ var articleOne={
     content: ` This is the content for my first article.....!!!`
 };
 function createTemplate (data) {
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    
 var htmlTemplate=
     `<html>
     <head>
