63809914f78288047bf4d0826c62bf8b2706ee85
devikaparvathy <31448741+devikaparvathy@users.noreply.github.com>
2017-09-05 12:34:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,6 +19,6 @@ button.onclick = function(){
   };
 
   //make the request
-  request.open('GET','http://devikaparvathym.imad.hasura-app.io/counter',true);
+  request.open('GET', 'http://devikaparvathym.imad.hasura-app.io/counter',true);
   request.send(null);
 };
