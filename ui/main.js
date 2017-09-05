b0f3e26d730b65b94372fe42eb4abbc2c2c931cb
devikaparvathy <31448741+devikaparvathy@users.noreply.github.com>
2017-09-05 12:36:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ button.onclick = function(){
   var request = new XMLHttpRequest();
   //capture the response and store it in a variable
   request.onreadystatechange = function(){
-      if (request.readyState===XMLHttpRequest.Done) {
+      if (request.readyState===XMLHttpRequest.DONE) {
           //Take some action
           if (request.status===200){
              var counter = request.responseText;
