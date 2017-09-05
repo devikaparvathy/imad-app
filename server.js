6bd9c375c0b02d012d63de1abe846891735a89b8
devikaparvathy <31448741+devikaparvathy@users.noreply.github.com>
2017-09-05 14:19:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/submit-name/:name',function (req, res) {
    var name = req.params.name;
    names.push(name);
    //JSON
-   res.send(JSON.stringfy(names));
+   res.send(JSON.stringify(names));
    
 });
 
